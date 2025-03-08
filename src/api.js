0028b067ed42ae3e182c4666cef26ffa01d0509a
OakJkpG <jakapong2547@gmail.com>
2025-03-08 20:16:37 +0700

    update

--- src/api.js
+++ src/api.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
+import { createClient } from '@supabase/supabase-js';
 
-const API_URL = 'https://your-backend-url.com/api';
+const supabaseUrl = 'https://jesymqwwoxinovuxsibt.supabase.co';  // URL ของ Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Implc3ltcXd3b3hpbm92dXhzaWJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0MjczOTgsImV4cCI6MjA1NzAwMzM5OH0.fimoy-HUJAxf9q3LnfkyuvRB7dIDfEfeXTw4emrjO5c';  // คีย์สำหรับเชื่อมต่อ (ไปที่ Supabase Dashboard แล้วหา API Key)
 
-export const getBooks = async () => {
-  const response = await axios.get(`${API_URL}/books/`);
-  return response.data;
-};
+export const supabase = createClient(supabaseUrl, supabaseKey);
